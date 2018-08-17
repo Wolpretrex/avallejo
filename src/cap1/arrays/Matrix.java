4d929708c002aa95978ba1e7a50c7b2d1aa2012d
Wolpretrex <alejandro.vallejo@utp.edu.co>
2018-08-17 02:08:57 -0500

    Iniciamos

---
+++ src/cap1/arrays/Matrix.java
@@ -0,0 +1,5 @@
+package cap1.arrays;
+
+public class Matrix {
+
+}
---
+++ src/cap1/arrays/Vector.java
@@ -0,0 +1,55 @@
+package cap1.arrays;
+
+public class Vector {
+	// Step 1. Create attributes
+	private int size;
+	private int[] elements;
+	
+	// Create the builder
+//
+	public Vector(int newSize) {
+		System.out.println("Creating vector with size="+newSize);
+		this.size = newSize;
+		this.elements = new int[newSize];
+	}
+
+	// Step 2. Create methods
+	// Step 2.1. Generate encapsulation methods
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	public int[] getElements() {
+		return elements;
+	}
+
+	public void setElements(int[] elements) {
+		this.elements = elements;
+	}
+	// Step 2.2. Create CRUD methods
+	public int[] insertElement(int element, int position) {
+		this.elements[position] = element;		
+		return this.elements;
+	}
+	
+	public int searchElement(int position) {
+		return this.elements[position];
+	}
+	
+	public int[] updateElement(int element, int position) {
+		 this.elements[position] = element;
+		return this.elements;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
